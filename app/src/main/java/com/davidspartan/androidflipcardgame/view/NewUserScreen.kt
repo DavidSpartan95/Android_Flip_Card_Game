fc8f879af9ef49e9c9b4245d2bc6f1d11a662271
DavidSpartan95 <davidulwhan@hotmail.com>
2025-01-27 09:58:21 +0100

    feat: Add darkened background and dismiss logic for Popup in NewUserScreen feat: In NewUserScreen User can now add more users

--- app/src/main/java/com/davidspartan/androidflipcardgame/view/NewUserScreen.kt
+++ app/src/main/java/com/davidspartan/androidflipcardgame/view/NewUserScreen.kt
@@ -1,6 +1,7 @@
 package com.davidspartan.androidflipcardgame.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,8 +28,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.navigation.NavHostController
+import com.davidspartan.androidflipcardgame.view.components.OptionButton
 import com.davidspartan.androidflipcardgame.view.navigation.Home
 import com.davidspartan.androidflipcardgame.viewmodel.UserRepositoryViewModel
 
@@ -36,81 +43,113 @@ fun NewUserScreen(
     viewModel: UserRepositoryViewModel
 ) {
     val users by viewModel.users.collectAsState(initial = emptyList()) // Provide an initial value
-    val selectedUser by viewModel.selectedUser.collectAsState(initial = null)
+    val showPopup = remember { mutableStateOf(false) } // Track if popup is visible
 
-    var newUserName by remember { mutableStateOf("username") }
-
-    Column(
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+
+
     ) {
-        Text("New User Screen", style = MaterialTheme.typography.bodyMedium)
 
-        if (selectedUser != null) {
-            Text("Selected User: ${selectedUser?.name}")
-        }
-
-        if (users.isNotEmpty()) {
-            Column {
-                users.forEach { user ->
-                    Box(
-                        modifier = Modifier
-                            .padding(8.dp) // Add spacing around each rectangle
-                            .background(
-                                color = MaterialTheme.colorScheme.primary, // Background color with some transparency
-                                shape = RoundedCornerShape(8.dp) // Rounded corners for the rectangle
-                            )
-                            .padding(16.dp) // Padding inside the rectangle
-                            .clickable {
-                                viewModel.selectUser(user)
-                                navController.navigate(Home)
-                            }
-                    ) {
-                        Text(
-                            text = user.name,
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = MaterialTheme.colorScheme.onPrimary // Text color contrasting the background
+        Column(
+            Modifier
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                showPopup.value = true
+            }) {
+                Text("Create New User")
+            }
+            users.forEach { user ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(8.dp) // Add spacing around each rectangle
+                        .background(
+                            color = MaterialTheme.colorScheme.primary, // Background color with some transparency
+                            shape = RoundedCornerShape(8.dp) // Rounded corners for the rectangle
                         )
-                    }
+                        .padding(8.dp) // Padding inside the rectangle
+                        .clickable {
+                            viewModel.selectUser(user)
+                            navController.navigate(Home)
+                        }
+                        .width(250.dp)
+                ) {
+                    Text(
+                        text = user.name,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onPrimary // Text color contrasting the background
+                    )
                 }
             }
-        } else {
-            // Display a form to create a new user if no users are found
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+        }
+
+
+        // Show popup with darkened or blurred background
+        if (showPopup.value) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.5f)) // Darkened background
+                    .clickable { showPopup.value = false } // Dismiss popup on background click
             ) {
-                Text("No users found. Please create a new user.")
+                PopupBox(viewModel, onDismiss = { showPopup.value = false })
+            }
+        }
 
-                Spacer(modifier = Modifier.height(16.dp))
+    }
+}
 
-                // TextInput for the new user name
-                OutlinedTextField(
-                    value = newUserName,
-                    onValueChange = { newUserName = it },
-                    label = { Text("User Name") },
-                    modifier = Modifier.fillMaxWidth(),
-                    singleLine = true
-                )
+@Composable
+fun PopupBox(
+    viewModel: UserRepositoryViewModel,
+    onDismiss: () -> Unit
+) {
+    var newUserName by remember { mutableStateOf("das") }
 
-                Spacer(modifier = Modifier.height(16.dp))
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            Modifier
+                .background(color = Color.Magenta, shape = RoundedCornerShape(8.dp))
+                .size(250.dp)
+                .fillMaxWidth()
+                .padding(2.dp)
+                .border(width = 1.dp, color = Color.White, shape = RoundedCornerShape(8.dp))
+                .padding(20.dp)
+        ) {
+            Text("No users found. Please create a new user.")
 
-                // Button to create the user
-                Button(
-                    onClick = {
-                        if (newUserName.isNotEmpty()) {
-                            viewModel.addUser(
-                                name = newUserName
-                            ) // Call the function to add the user
-                            //navController.navigate(Home) // Navigate to the Home screen after adding
-                        }
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Create User")
-                }
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = newUserName,
+                onValueChange = { newUserName = it },
+                label = { Text("User Name") },
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    if (newUserName.isNotEmpty()) {
+                        viewModel.addUser(name = newUserName)
+                        onDismiss()
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Create User")
             }
         }
     }
