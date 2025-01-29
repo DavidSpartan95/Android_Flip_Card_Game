8992da4f92016a39cb0e84aac6150d9f2e385de9
DavidSpartan95 <davidulwhan@hotmail.com>
2025-01-29 11:27:47 +0100

    UI: Hides systemBar and lock orientation to portrait

--- app/src/main/java/com/davidspartan/androidflipcardgame/MainActivity.kt
+++ app/src/main/java/com/davidspartan/androidflipcardgame/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.davidspartan.androidflipcardgame
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.compose.rememberNavController
 import com.davidspartan.androidflipcardgame.view.navigation.NavHost
 import com.davidspartan.androidflipcardgame.viewmodel.UserRepositoryViewModel
@@ -14,6 +18,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Lock orientation to portrait
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+        insetsController.apply {
+            hide(WindowInsetsCompat.Type.navigationBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
         setContent {
             val navController = rememberNavController()
             val viewModel = UserRepositoryViewModel()
