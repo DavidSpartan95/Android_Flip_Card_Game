0dc9b321975aa71d073982562277cf7aef871e27
DavidSpartan95 <davidulwhan@hotmail.com>
2025-03-27 08:56:43 +0100

    Fix: Added button to UserNotLoggedIn to revert user

--- app/src/main/java/com/davidspartan/androidflipcardgame/view/components/UserNotLoggedIn.kt
+++ app/src/main/java/com/davidspartan/androidflipcardgame/view/components/UserNotLoggedIn.kt
@@ -1,16 +1,25 @@
 package com.davidspartan.androidflipcardgame.view.components
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavHostController
 import com.davidspartan.androidflipcardgame.view.navigation.NewUser
+import com.davidspartan.database.realm.AllThemes
 
 @Composable
 fun UserNotLoggedInScreen(navController: NavHostController) {
-    LaunchedEffect(true){
-        // This will pop the stack and navigate to the login screen
+
+    ThemedText(
+        text = "USER HAS BEEN LOGGED OUT",
+        theme = AllThemes[0]
+    )
+
+    Button(onClick = {
         navController.navigate(NewUser) {
             popUpTo(navController.graph.startDestinationId) { inclusive = true }
         }
+    }) {
+        Text(text = "TAKE ME BACK TO LOGIN SCREEN")
     }
 }
