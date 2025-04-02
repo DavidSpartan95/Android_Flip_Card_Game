c4da4ee485f7252deb92c431f67b1c9a9a3d7901
DavidSpartan95 <davidulwhan@hotmail.com>
2025-04-02 17:16:17 +0200

    Changed: Theme Color

--- core/database/src/main/java/com/davidspartan/database/realm/ThemeList.kt
+++ core/database/src/main/java/com/davidspartan/database/realm/ThemeList.kt
@@ -5,42 +5,42 @@ val AllThemes = listOf(
     Theme().apply {
         name = "Default"
         primaryHexColor = "#FF923FFF"
-        secondaryHexColor = "#DCE2F0"
+        secondaryHexColor = "#7410CB"
         textHexColor = "#FF923FFF"
         price = 0
     },
     Theme().apply {
         name = "Forest"
         primaryHexColor = "#07553B"
-        secondaryHexColor = "#CED46A"
+        secondaryHexColor = "#06402c"
         textHexColor = "#07553B"
         price = 10
     },
     Theme().apply {
         name = "Lavender"
         primaryHexColor = "#50586C"
-        secondaryHexColor = "#DCE2F0"
+        secondaryHexColor = "#373e4f"
         textHexColor = "#50586C"
         price = 20
     },
     Theme().apply {
         name = "Wood"
-        primaryHexColor = "#F9EBDE"
-        secondaryHexColor = "#815854"
+        primaryHexColor = "#825d5a"
+        secondaryHexColor = "#593f3d"
         textHexColor = "#F9EBDE"
         price = 30
     },
     Theme().apply {
         name = "Thunder"
-        primaryHexColor = "#101820"
-        secondaryHexColor = "#FEE715"
+        primaryHexColor = "#b38c0e"
+        secondaryHexColor = "#755c08"
         textHexColor = "#101820"
         price = 30
     },
     Theme().apply {
         name = "Lava"
         primaryHexColor = "#A4193D"
-        secondaryHexColor = "#FFDFB9"
+        secondaryHexColor = "#7a122d"
         textHexColor = "#A4193D"
         price = 30
     }
