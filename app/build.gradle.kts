14b1896ffe3acdc7d294b4ca5d32202bd7e310b3
DavidSpartan95 <davidulwhan@hotmail.com>
2025-04-03 21:01:47 +0200

    New Version Code and Name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.davidspartan.androidflipcardgame"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
